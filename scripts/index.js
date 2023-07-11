dcd046b4b81a9397abbe81e76d7394b0f35ee2fa
Paolo Leucio Manca <paolomanca1990@gmail.com>
2023-07-11 11:21:47 +0200

    home

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,21 @@
 const firstRow = document.getElementById("first-row")
-const Url = "https://striveschool-api.herokuapp.com/api/deezer/album/"
+const secondRow = document.getElementById("second-row")
+const albumUrl = "https://striveschool-api.herokuapp.com/api/deezer/album/"
 
-const choosenAlbums = [975682572, 151684662, 1247956402, 113948126, 2351917235, 67860535]
+const choosenAlbums = [90271, 113477, 233425292, 78536192, 333896337, 121532]
+const forYouAlbum = [90271, 113477, 233425292, 78536192]
+// const randomAlbums = function(){
+//     for (let i=0; i<6; i++){
+//         let num = Math.floor(Math.random()*200000000) + 150000000
+//         choosenAlbums.push(num)
+//     }
+// }
 
+// randomAlbums()
 
 const getAlbum = function() {
     choosenAlbums.forEach((el) =>{
-        fetch(Url + el)
+        fetch(albumUrl + el)
             .then((res) => {
                 if (res.ok) {
                     return res.json()
@@ -18,9 +27,9 @@ const getAlbum = function() {
                 let newCol = document.createElement("div")
                 newCol.classList.add("col", "col-6", "col-lg-4")
                 newCol.innerHTML= `
-                <div class="d-flex align-items-center rounded border">
+                <div class="d-flex align-items-center rounded shadow">
                 <img class="rounded-start" src="${data.cover_small}" alt="cover" />
-                <h4 class="m-0 ps-1"> ${data.title}</h4>
+                <h6 class="m-0 ps-1"> ${data.title}</h6>
                 </div>
                 `
 
@@ -30,12 +39,41 @@ const getAlbum = function() {
                 console.log("ERR", err)
             }) 
         }
-    )}
+    )
+}
 
+getAlbum()
 
-    getAlbum()
-// const getAlbum = funtion (){
-//     choosenAlbums.forEach((el) => {
+const createForYou = function() {
+    forYouAlbum.forEach((el,i) =>{
+        fetch(albumUrl + el)
+            .then((res) => {
+                if (res.ok) {
+                    return res.json()
+                } else {
+                    throw new Error ("Errore nel recupero dei dati")
+                }
+            })
+            .then((data,) => {
+                let newCol = document.createElement("div")
+                newCol.classList.add("col", "col-6", "col-lg-3")
+                newCol.innerHTML= `
+                <div class="card rounded-2 h-100 p-3 text-bg-dark">
+                    <img src="${data.cover_medium}" class="card-img rounded-2" alt="cover">
+                    <div class="card-body p-0 mt-2">
+                        <h5 class="card-title p-0">Daily Mix ${i+1}</h5>
+                        <p class="card-text p-0">${data.title}</p>
+                    </div>
+                </div>
+                `
 
-//     })
-// }
+                secondRow.appendChild(newCol)
+            }) 
+            .catch((err) => {
+                console.log("ERR", err)
+            }) 
+        }
+    )
+}
+
+createForYou()
