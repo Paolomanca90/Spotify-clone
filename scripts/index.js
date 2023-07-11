5410057e82fc9d8eb603a53a243dcdbf1a1337f4
Paolo Leucio Manca <paolomanca1990@gmail.com>
2023-07-11 15:04:25 +0200

    home

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,58 @@
 const firstRow = document.getElementById("first-row")
 const secondRow = document.getElementById("second-row")
+const thirdRow = document.getElementById("third-row")
+const fourthRow = document.getElementById("fourth-row")
+const searchUrl = "https://striveschool-api.herokuapp.com/api/deezer/search?q=rock"
 const albumUrl = "https://striveschool-api.herokuapp.com/api/deezer/album/"
 
-const choosenAlbums = [90271, 113477, 233425292, 78536192, 333896337, 121532]
-const forYouAlbum = [90271, 113477, 233425292, 78536192]
-// const randomAlbums = function(){
-//     for (let i=0; i<6; i++){
-//         let num = Math.floor(Math.random()*200000000) + 150000000
-//         choosenAlbums.push(num)
-//     }
-// }
+const choosenAlbums = []
+const forYouAlbum = []
+const mixAlbum = []
+const recentAlbum = []
 
-// randomAlbums()
+const randomAlbums = function(){
+    fetch(searchUrl)
+        .then((res)=>{
+            if(res.ok){
+                return res.json()
+            }else{
+                throw new Error()
+            }
+        })
+        .then((data)=>{
+            for (let i=0; i<6; i++){
+                let num = Math.floor(Math.random()*data.data.length)
+                choosenAlbums.push(data.data[num].album.id)
+            }
+            getAlbum()  
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+}
+randomAlbums()
+
+const randomForYou = function(){
+    fetch(searchUrl)
+        .then((res)=>{
+            if(res.ok){
+                return res.json()
+            }else{
+                throw new Error()
+            }
+        })
+        .then((data)=>{
+            for (let i=0; i<4; i++){
+                let num = Math.floor(Math.random()*data.data.length)
+                forYouAlbum.push(data.data[num].album.id)   
+            } 
+            createForYou() 
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+}
+randomForYou()
 
 const getAlbum = function() {
     choosenAlbums.forEach((el) =>{
@@ -42,8 +83,6 @@ const getAlbum = function() {
     )
 }
 
-getAlbum()
-
 const createForYou = function() {
     forYouAlbum.forEach((el,i) =>{
         fetch(albumUrl + el)
@@ -54,7 +93,7 @@ const createForYou = function() {
                     throw new Error ("Errore nel recupero dei dati")
                 }
             })
-            .then((data,) => {
+            .then((data) => {
                 let newCol = document.createElement("div")
                 newCol.classList.add("col", "col-6", "col-lg-3")
                 newCol.innerHTML= `
@@ -76,4 +115,110 @@ const createForYou = function() {
     )
 }
 
-createForYou()
+const mixAlbums = function(){
+    fetch(searchUrl)
+        .then((res)=>{
+            if(res.ok){
+                return res.json()
+            }else{
+                throw new Error()
+            }
+        })
+        .then((data)=>{
+            for (let i=0; i<4; i++){
+                let num = Math.floor(Math.random()*data.data.length)
+                mixAlbum.push(data.data[num].album.id)   
+            } 
+            mixForYou() 
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+}
+mixAlbums()
+
+const mixForYou = function() {
+    mixAlbum.forEach((el) =>{
+        fetch(albumUrl + el)
+            .then((res) => {
+                if (res.ok) {
+                    return res.json()
+                } else {
+                    throw new Error ("Errore nel recupero dei dati")
+                }
+            })
+            .then((data) => {
+                let newCol = document.createElement("div")
+                newCol.classList.add("col", "col-6", "col-lg-3")
+                newCol.innerHTML= `
+                <div class="card rounded-2 h-100 p-3 text-bg-dark">
+                    <img src="${data.cover_medium}" class="card-img rounded-2" alt="cover">
+                    <div class="card-body p-0 mt-2">
+                        <h5 class="card-title p-0">Mix ${data.artist.name}</h5>
+                        <p class="card-text p-0">${data.title}</p>
+                    </div>
+                </div>
+                `
+
+                thirdRow.appendChild(newCol)
+            }) 
+            .catch((err) => {
+                console.log("ERR", err)
+            }) 
+        }
+    )
+}
+
+const recentAlbums = function(){
+    fetch(searchUrl)
+        .then((res)=>{
+            if(res.ok){
+                return res.json()
+            }else{
+                throw new Error()
+            }
+        })
+        .then((data)=>{
+            for (let i=0; i<4; i++){
+                let num = Math.floor(Math.random()*data.data.length)
+                recentAlbum.push(data.data[num].album.id)   
+            } 
+            recentForYou() 
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+}
+recentAlbums()
+
+const recentForYou = function() {
+    recentAlbum.forEach((el) =>{
+        fetch(albumUrl + el)
+            .then((res) => {
+                if (res.ok) {
+                    return res.json()
+                } else {
+                    throw new Error ("Errore nel recupero dei dati")
+                }
+            })
+            .then((data) => {
+                let newCol = document.createElement("div")
+                newCol.classList.add("col", "col-6", "col-lg-3")
+                newCol.innerHTML= `
+                <div class="card rounded-2 h-100 p-3 text-bg-dark">
+                    <img src="${data.cover_medium}" class="card-img rounded-2" alt="cover">
+                    <div class="card-body p-0 mt-2">
+                        <h5 class="card-title p-0">${data.artist.name}</h5>
+                        <p class="card-text p-0">${data.title}</p>
+                    </div>
+                </div>
+                `
+
+                fourthRow.appendChild(newCol)
+            }) 
+            .catch((err) => {
+                console.log("ERR", err)
+            }) 
+        }
+    )
+}
