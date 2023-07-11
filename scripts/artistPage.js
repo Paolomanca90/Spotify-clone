5e797d2e00a4c18544002067a06be114cf188708
Simone Potenza <simonepotenzatr@gmail.com>
2023-07-11 16:37:47 +0200

    Inizializing Js artists

--- scripts/artistPage.js
+++ scripts/artistPage.js
@@ -1,4 +1,33 @@
-const Url =  "https://striveschool-api.herokuapp.com/api/deezer/artist/0003"
+const UrlArtist =  "https://striveschool-api.herokuapp.com/api/deezer/album/"
+
+const coverImgRef = document.getElementById("coverImageArtist")
+const artistNameRef = document.getElementById("artistName")
+const monthlyListenersRef = document.getElementById("monthlyListeners")
+
+const urlSearchParams = new URLSearchParams(window.location.search)
+const artistId = urlSearchParams.get("artistId")
+
+
+const getArtist = function(artistId){
+    fetch(UrlArtist + artistId)
+    .then((artist) => {
+        if(artist.ok){
+            return artist.json()
+        } else {
+            throw new Error ("Errore nella chiamata API")
+        }
+    })
+    .then ((artists) => {
+        console.log("artists", artists)
+        artistNameRef.innerHTML=`
+        <h2>${artists.name}</h2>
+        `
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+}
+
 
 
 const playlist = ["culetto 2021", "Frah Quintale", "Be the young", "minecraft&nintendo switch", "Trallallero", "saggio vibes", "2021 lol", "Come trovare gli album", "Appendi sto child", "Che schifo Bootstrap", "Le bestemmine degli Epicoders", "Fetchati questo", "Francesco Guccini Mix", "Lucio Dalla e non fare la preziosa", "Bombe a confindustria", "Cercasi decimo per il calcetto", "Siamo tutti antifascisti", "Le magliette belle di Paolo", "La partita IVA di Paolo", "Sole a mezzanotte di Nino", "Torneo di calcetto di Antonio", "Pullman di Berlusconi", "Vacanza a Gubbio", "Saga completa Silent Hill", "Budokai Tenkaichi 2", "Radio Los Santos"]
@@ -19,28 +48,7 @@ const showPlaylist = function() {
 
 
 
-const getData = function(){
-    fetch(Url, {
-        headers: {
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGE3ZWZmYzEyYjUwYzAwMTQ5ZTUxODEiLCJpYXQiOjE2ODg5ODMzNzYsImV4cCI6MTY5MDE5Mjk3Nn0.lP7ejFmSeu5EV9ixrJDPJmBkMJ_3UPKq20ukGcuVC9E",
-        "Content-Type" : "application/json",
-        }
-        })
-    .then((res) => {
-        if(res.ok){
-            return res.json()
-        } else {
-            throw new Error ("Errore nella chiamata API")
-        }
-    })
-    .then ((artists) => {
-        console.log("artists", artists)
-    })
-    .catch((err) => {
-        console.log(err)
-    })
-}
 
 
-getData()
+getArtist()
 showPlaylist()
