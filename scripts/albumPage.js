b2495442c40387e668da8620c2f2e1c07b2b19e5
Simone Potenza <simonepotenzatr@gmail.com>
2023-07-13 17:54:00 +0200

    Finished the album page

--- scripts/albumPage.js
+++ scripts/albumPage.js
@@ -1,4 +1,5 @@
 const UrlAlbums = "https://striveschool-api.herokuapp.com/api/deezer/album/";
+let myUrl = "https://striveschool-api.herokuapp.com/api/deezer/search?q="
 const addressBarContent = new URLSearchParams(location.search);
 const albumId = addressBarContent.get("id");
 const coverAlbumRef = document.getElementById("cover-album")
@@ -57,3 +58,46 @@ fetch(UrlAlbums + albumId)
     .catch((err)=>{
         console.log(err)
     })
+
+    const playlist = [
+        "culetto 2021",
+        "Frah Quintale",
+        "Be the young",
+        "minecraft&nintendo switch",
+        "Trallallero",
+        "saggio vibes",
+        "2021 lol",
+        "Come trovare gli album",
+        "Appendi sto child",
+        "Che schifo Bootstrap",
+        "Le bestemmine degli Epicoders",
+        "Fetchati questo",
+        "Francesco Guccini Mix",
+        "Lucio Dalla e non fare la preziosa",
+        "Bombe a confindustria",
+        "Cercasi decimo per il calcetto",
+        "Siamo tutti antifascisti",
+        "Le magliette belle di Paolo",
+        "La partita IVA di Paolo",
+        "Sole a mezzanotte di Nino",
+        "Torneo di calcetto di Antonio",
+        "Pullman di Berlusconi",
+        "Vacanza a Gubbio",
+        "Saga completa Silent Hill",
+        "Budokai Tenkaichi 2",
+        "Radio Los Santos",
+    ];
+
+    const showPlaylist = function () {
+        let ulPlaylist = document.getElementById("lista-playlist");
+        playlist.forEach((playlist) => {
+            let newPlaylist = document.createElement("li");
+            newPlaylist.classList.add("text-secondary");
+            newPlaylist.innerHTML = `
+                <a href="#" class="link-secondary link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover ms-2">${playlist}</a>
+                `;
+            ulPlaylist.appendChild(newPlaylist);
+        });
+    };
+
+    showPlaylist()
