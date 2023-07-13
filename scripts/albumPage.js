0a3adeffbf67ac2b8df4dec266315395d52f2bd3
Simone Potenza <simonepotenzatr@gmail.com>
2023-07-13 12:38:00 +0200

    Js for Album Page

--- scripts/albumPage.js
+++ scripts/albumPage.js
@@ -1,6 +1,9 @@
-const UrlAlbums = "https://striveschool-api.herokuapp.com/api/deezer/albums/";
+const UrlAlbums = "https://striveschool-api.herokuapp.com/api/deezer/album/";
 const addressBarContent = new URLSearchParams(location.search);
 const albumId = addressBarContent.get("id");
+const coverAlbumRef = document.getElementById("cover-album")
+const albumNameRef = document.getElementById("album-name")
+const albumInfoRef = document.getElementById("album-info")
 
 fetch(UrlAlbums + albumId)
     .then((data) => {
@@ -11,7 +14,14 @@ fetch(UrlAlbums + albumId)
         }
     })
     .then((album)=>{
-        console.log("album", album)
+        console.log("album", album.cover_medium)
+        coverAlbumRef.style.backgroundImage = `url(${album.cover_medium})`
+        coverAlbumRef.style.backgroundSize = "cover"
+        coverAlbumRef.style.height = "250px"
+        coverAlbumRef.style.width = "250px"
+        albumNameRef.innerText = `${album.title}`
+        albumInfoRef.innerText = `${album.artist.name} • ${album.release_date} • ${album.nb_tracks} brani`
+        
     })
     .catch((err)=>{
         console.log(err)
