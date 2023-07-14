08b1747eae5befeca8bc7a94c2f958d75d8b003e
Paolo Leucio Manca <paolomanca1990@gmail.com>
2023-07-14 12:54:42 +0200

    home

--- scripts/albumPage.js
+++ scripts/albumPage.js
@@ -1,5 +1,7 @@
 const UrlAlbums = "https://striveschool-api.herokuapp.com/api/deezer/album/"
 let myUrl = "https://striveschool-api.herokuapp.com/api/deezer/search?q="
+let searchUrl =
+  "https://striveschool-api.herokuapp.com/api/deezer/search?q=rock"
 const addressBarContent = new URLSearchParams(location.search)
 const albumId = addressBarContent.get("id")
 const coverAlbumRef = document.getElementById("cover-album")
@@ -182,19 +184,19 @@ fetch(UrlAlbums + albumId)
         "Radio Los Santos",
     ];
 
-    const showPlaylist = function () {
-        let ulPlaylist = document.getElementById("lista-playlist");
-        playlist.forEach((playlist) => {
-            let newPlaylist = document.createElement("li");
-            newPlaylist.classList.add("text-secondary");
-            newPlaylist.innerHTML = `
-                <a href="#" class="link-secondary link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover ms-2">${playlist}</a>
-                `;
-            ulPlaylist.appendChild(newPlaylist);
-        });
-    };
+    // const showPlaylist = function () {
+    //     let ulPlaylist = document.getElementById("lista-playlist");
+    //     playlist.forEach((playlist) => {
+    //         let newPlaylist = document.createElement("li");
+    //         newPlaylist.classList.add("text-secondary");
+    //         newPlaylist.innerHTML = `
+    //             <a href="#" class="link-secondary link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover ms-2">${playlist}</a>
+    //             `;
+    //         ulPlaylist.appendChild(newPlaylist);
+    //     });
+    // };
 
-    showPlaylist()
+    // showPlaylist()
 
     const iconX = document.querySelector(".bi-x-lg");
     const aside = document.querySelector("aside");
@@ -259,3 +261,52 @@ fetch(UrlAlbums + albumId)
     
     const yearSpan = document.querySelector('#year')
     yearSpan.innerHTML = myYear
+
+    const showPlaylist = function (playlist) {
+      console.log(playlist)
+      let ulPlaylist = document.getElementById("lista-playlist")
+      ulPlaylist.innerHTML = ""
+      playlist.forEach((playlistItem) => {
+        let newPlaylist = document.createElement("li")
+        newPlaylist.classList.add(
+          "text-secondary",
+          "d-flex",
+          "align-items-center",
+          "mb-3"
+        )
+        newPlaylist.innerHTML = `
+          <img class="imgPlaylist" src="${playlistItem.cover_small}" alt="cover"/>
+          <div class="ps-2 d-flex flex-column justify-content-center">
+          <p class="artistPlaylist text-light m-0 p-0">${playlistItem.artist}</p> 
+          <a href="#" class="artistPlaylist link-secondary link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover">${playlistItem.title}</a>
+          </div>
+          `
+        ulPlaylist.appendChild(newPlaylist)
+      })
+    }
+    
+    const createPlaylist = function () {
+      fetch(searchUrl)
+        .then((res) => {
+          if (res.ok) {
+            return res.json()
+          } else {
+            throw new Error("Errore nel recupero dei dati")
+          }
+        })
+        .then((data) => {
+          const albumList = data.data.map((item) => {
+            return {
+              title: item.album.title,
+              artist: item.artist.name,
+              cover_small: item.album.cover_small,
+            }
+          })
+          showPlaylist(albumList)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+    
+    createPlaylist()
--- scripts/index.js
+++ scripts/index.js
@@ -89,7 +89,7 @@ const showPlaylist = function (playlist) {
     )
     newPlaylist.innerHTML = `
       <img class="imgPlaylist" src="${playlistItem.cover_small}" alt="cover"/>
-      <div class="ps-2">
+      <div class="ps-2 d-flex flex-column justify-content-center">
       <p class="artistPlaylist text-light m-0 p-0">${playlistItem.artist}</p> 
       <a href="#" class="artistPlaylist link-secondary link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover">${playlistItem.title}</a>
       </div>
@@ -98,7 +98,7 @@ const showPlaylist = function (playlist) {
   })
 }
 
-const createPlaylist = function (searchUrl) {
+const createPlaylist = function () {
   console.log(searchUrl)
   fetch(searchUrl)
     .then((res) => {
@@ -123,7 +123,7 @@ const createPlaylist = function (searchUrl) {
     })
 }
 
-createPlaylist(searchUrl)
+createPlaylist()
 
 const getAlbum = function () {
   choosenAlbums.forEach((el) => {
